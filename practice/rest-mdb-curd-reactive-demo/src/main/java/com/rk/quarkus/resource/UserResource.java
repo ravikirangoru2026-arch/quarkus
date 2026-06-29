212b233de46c493fd4d90309087c8bfa02e2b66e
RavikiranGoru2026 <ravikirangoru2026@gmail.com>
2026-06-29 08:02:23 +0530

    added openapi/swagger and logs

--- practice/rest-mdb-curd-reactive-demo/src/main/java/com/rk/quarkus/resource/UserResource.java
+++ practice/rest-mdb-curd-reactive-demo/src/main/java/com/rk/quarkus/resource/UserResource.java
@@ -2,10 +2,12 @@
 
 import java.util.List;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import com.rk.quarkus.service.UserService;
 
-import io.quarkus.hibernate.reactive.panache.common.WithSession;
-import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
@@ -21,40 +23,42 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "User API", description = "Operations on users")
 public class UserResource {
 
 	@Inject
 	UserService userService;
 
 	@GET
-	@WithSession
+	@Operation(summary = "List all users", description = "Returns all users in the system")
 	public Uni<List<com.rk.quarkus.dto.User>> listAll() {
+		Log.info("Get all users details");
 		return userService.listAll();
 	}
 
 	@GET
 	@Path("/{id}")
-	@WithSession
+	@Operation(summary = "Fetch user", description = "Fetch user by id")
 	public Uni<com.rk.quarkus.dto.User> findById(@PathParam("id") Long id) {
 		return userService.findById(id);
 	}
 
 	@POST
-	@WithTransaction
+	@Operation(summary = "Create user", description = "Creates a new user")
 	public Uni<com.rk.quarkus.dto.User> create(com.rk.quarkus.dto.User user) {
 		return userService.create(user);
 	}
 
 	@PUT
 	@Path("/{id}")
-	@WithTransaction
+	@Operation(summary = "Update user", description = "Update user")
 	public Uni<com.rk.quarkus.dto.User> update(@PathParam("id") Long id, com.rk.quarkus.dto.User updated) {
 		return userService.update(id, updated);
 	}
 
 	@DELETE
 	@Path("/{id}")
-	@WithTransaction
+	@Operation(summary = "Delete user", description = "Delete user")
 	public Uni<Boolean> delete(@PathParam("id") Long id) {
 		return userService.delete(id);
 	}
--- practice/rest-mdb-curd-reactive-demo/src/main/java/com/rk/quarkus/service/UserService.java
+++ practice/rest-mdb-curd-reactive-demo/src/main/java/com/rk/quarkus/service/UserService.java
@@ -10,6 +10,7 @@ import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.hibernate.reactive.panache.common.WithSession;
 import io.quarkus.hibernate.reactive.panache.common.WithSessionOnDemand;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -22,21 +23,29 @@ public class UserService {
 	@Inject
 	UserRepository repository;
 
+	@WithSessionOnDemand
 	public Uni<List<com.rk.quarkus.dto.User>> listAll() {
+		Log.info("Get all users- started");
 		return repository.listAll().onItem().transform(users -> users.stream().map(UserMapper::toDTO).toList());
 	}
 
+	@WithSessionOnDemand
 	public Uni<com.rk.quarkus.dto.User> findById(Long id) {
+		Log.info("Find user by id :{}"+ id);
 		return repository.findById(id).onItem().ifNull().failWith(new NotFoundException()).onItem()
 				.transform(UserMapper::toDTO);
 	}
 
+	@WithTransaction
 	public Uni<com.rk.quarkus.dto.User> create(com.rk.quarkus.dto.User user) {
 		User entity = UserMapper.toEntity(user);
+		Log.info("Create user :"+entity);
 		return repository.persist(entity).replaceWith(UserMapper.toDTO(entity));
 	}
 
+	@WithTransaction
 	public Uni<com.rk.quarkus.dto.User> update(Long id, com.rk.quarkus.dto.User updated) {
+		Log.info("Update user for id :"+id+" user details :"+updated);
 		return repository.findById(id).onItem().ifNull().failWith(new NotFoundException()).onItem()
 				.transform(entity -> {
 					UserMapper.updateEntity(entity, updated);
@@ -44,7 +53,9 @@ public class UserService {
 				});
 	}
 
+	@WithTransaction
 	public Uni<Boolean> delete(Long id) {
+		Log.info("Delete use by id :"+id);
 		return repository.deleteById(id);
 	}
 }
